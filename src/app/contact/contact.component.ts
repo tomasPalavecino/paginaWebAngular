a776b388ba88003ba0d46e84a943f5d3653b2131
tomasPalavecino <tomypalavecino17@gmail.com>
2024-04-14 17:29:24 -0300

    Validators

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+formularioContacto: FormGroup;
+
+constructor(private form: FormBuilder){
+  this.formularioContacto = this.form.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+  });
+}
+
+hasErrors(controlName: string, errorType: string){
+  return this.formularioContacto.get(controlName)?.hasError(errorType) &&
+  this.formularioContacto.get(controlName)?.touched;
+}
+
+  enviar(){
+    console.log(this.formularioContacto);
+  }
 }
