f43fa46e2c2f53acbc6bff60a34b640dbaf6fd3f
tomasPalavecino <tomypalavecino17@gmail.com>
2024-04-14 00:33:59 -0300

    implementando ngIf

--- src/app/products/products.mock.ts
+++ src/app/products/products.mock.ts
@@ -4,6 +4,7 @@ export const productsList :Product[] = [
     {id: 3, name: "Limpia vidrios", price: 7, description: "Tus vidrios transparentes"},
     {id: 4, name: "Quita grasa", price: 8, description: "Cocina tranquilo"},
     {id: 5, name: "Perfumina", price: 2, description: "El olor a campo mas realista"},
+    {id: 6, name: "Esponja", price: 4, description: ""},
 ]
 
 export interface Product{
